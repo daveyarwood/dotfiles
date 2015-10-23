e5349598ed98b642db5c128c93ce919e15b979bb
Dave Yarwood <dave.yarwood@gmail.com>
2015-10-23 08:48:48 -0400

    derp

--- config/fish/custom/docker.fish
+++ config/fish/custom/docker.fish
@@ -8,6 +8,6 @@ end
 # getting an OS X environment set up for Docker development.
 # TODO: look up how I got these variables, set them dynamically by running
 # the commands instead of having them hard-coded.
-set -gx DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
+set -gx DOCKER_CERT_PATH $HOME/.boot2docker/certs/boot2docker-vm
 set -gx DOCKER_HOST tcp://192.168.59.103:2376
-set -gx DOCKER_TLS_VERIFY=1
+set -gx DOCKER_TLS_VERIFY 1
