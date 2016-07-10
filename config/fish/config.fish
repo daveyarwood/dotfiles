d22c77d2fc46915f75e34f5ebafcfe926f6011fd
Dave Yarwood <dave.yarwood@gmail.com>
2016-07-10 16:10:29 -0400

    carp stuff

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -33,6 +33,7 @@ source $fish_runtime/custom/go.fish
 source $fish_runtime/custom/beets.fish
 source $fish_runtime/custom/php.fish
 source $fish_runtime/custom/pony.fish
+source $fish_runtime/custom/carp.fish
 
 # useful shell functions
 source $fish_runtime/custom/functions.fish
---
+++ config/fish/custom/carp.fish
@@ -0,0 +1,1 @@
+set -gx PATH $CODEDIR/Carp/bin $PATH
