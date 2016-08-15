caf5bed27d1f6594bbe97263a4e8295b7e9f2fc8
Dave Yarwood <dave.yarwood@gmail.com>
2016-08-15 10:33:22 -0400

    put LilyPond bin folder at the end of PATH so /usr/local/bin takes precedence
    Need guile to be /usr/local/bin/guile which is a newer version.

--- config/fish/custom/lilypond.fish
+++ config/fish/custom/lilypond.fish
@@ -1,3 +1,3 @@
 # NOTE: OS X specific
 # TODO: come up with some way to have different paths on different hosts
-set -gx PATH /Applications/LilyPond.app/Contents/Resources/bin $PATH
+set -gx PATH $PATH /Applications/LilyPond.app/Contents/Resources/bin
