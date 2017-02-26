3ee7aa48257882cb70a6c22af895084abb9cd969
Dave Yarwood <dave.yarwood@gmail.com>
2017-02-26 08:45:28 -0500

    rbenv partial yak shave

--- config/fish/custom/rbenv.fish
+++ config/fish/custom/rbenv.fish
@@ -1,3 +1,17 @@
-set -gx RBENV_ROOT /usr/local/var/rbenv
-source (rbenv init -|psub)
+if test (uname) = Darwin
+  set -gx RBENV_ROOT /usr/local/var/rbenv
+else
+  set -gx RBENV_ROOT $HOME/.rbenv
+end
+
+if status is-interactive
+  # on astrocat, the script that (rbenv init - | psub) emits is in bash instead
+  # of fish for some reason. I don't feel like shaving this yak, so just
+  # explicitly use bash if hostname is astrocat.
+  if test (hostname) = astrocat
+    bash (rbenv init - | psub)
+  else
+    source (rbenv init - | psub)
+  end
+end
 
