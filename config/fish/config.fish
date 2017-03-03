7f15356a1d3b25041c549f1766e4614f7d006509
Dave Yarwood <dave.yarwood@gmail.com>
2017-03-03 07:35:54 -0500

    ruby gem stuff

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -47,6 +47,7 @@ source $fish_runtime/custom/ocaml.fish
 source $fish_runtime/custom/mono.fish
 source $fish_runtime/custom/rust.fish
 source $fish_runtime/custom/ssh.fish
+source $fish_runtime/custom/ruby.fish
 
 # useful shell functions
 source $fish_runtime/custom/functions.fish
