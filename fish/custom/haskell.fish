b2c5edc72e673d93eec0ea03fdb25fd9dbec6988
Dave Yarwood <dave.yarwood@gmail.com>
2017-03-18 07:34:32 -0400

    linux-specific haskell setup
    Moving towards doing all my dev/hacking in a Linux VM instead of OS X. I've
    removed ghc/cabal/stack from my OS X machine and installed them in the Linux
    VM.
    
    If I ever install Haskell stuff on OS X again, I will adjust this setup to
    account for the OS X-specific paths as well.

--- fish/custom/haskell.fish
+++ fish/custom/haskell.fish
@@ -1,6 +1,16 @@
-# Add GHC 7.8.4 to the PATH, via http://ghcformacosx.github.io/
-set -gx GHC_DOT_APP /Applications/ghc-7.8.4.app
-if test -d $GHC_DOT_APP
-    set -gx PATH $HOME/.cabal/bin $GHC_DOT_APP/Contents/bin $PATH
+set -l cabal_path /opt/cabal
+set -l dot_cabal_path $HOME/.cabal
+set -l ghc_path /opt/ghc
+
+if test -d $ghc_path
+  set -gx PATH $ghc_path/bin $PATH
+end
+
+if test -d $cabal_path
+  set -gx PATH $cabal_path/bin $PATH
+end
+
+if test -d $dot_cabal_path
+  set -gx PATH $dot_cabal_path/bin $PATH
 end
 
