fcae4062fad63cf29d8c21a72494fb47eaa27680
Dave Yarwood <dave.yarwood@gmail.com>
2017-05-19 20:30:39 -0400

    Revert "ocaml config (generated by opam)"
    This reverts commit a79b13876074a181f54a6f4455772ef623e21859.

--- fish/custom/ocaml.fish
+++ fish/custom/ocaml.fish
@@ -1,2 +1,6 @@
-# OPAM configuration
-source /home/dave/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+set -l opam_dir $HOME/.opam/system/bin
+
+if test -d $opam_dir
+  set -gx PATH $opam_dir $PATH
+end
+
