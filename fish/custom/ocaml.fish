a79b13876074a181f54a6f4455772ef623e21859
Dave Yarwood <dave.yarwood@gmail.com>
2017-05-19 20:27:29 -0400

    ocaml config (generated by opam)

--- fish/custom/ocaml.fish
+++ fish/custom/ocaml.fish
@@ -1,6 +1,2 @@
-set -l opam_dir $HOME/.opam/system/bin
-
-if test -d $opam_dir
-  set -gx PATH $opam_dir $PATH
-end
-
+# OPAM configuration
+source /home/dave/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
