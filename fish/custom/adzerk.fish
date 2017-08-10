63532439562dddb5c41dff56348b3198b05e6ae7
Dave Yarwood <dave.yarwood@gmail.com>
2017-08-10 15:39:55 -0400

    completed migration to zerkenv

--- fish/custom/adzerk.fish
+++ fish/custom/adzerk.fish
@@ -1,21 +1,27 @@
-# TODO: remove
-function adzerk-env
-  if count $argv > /dev/null
-    if [ $argv[1] = prod ]
-      set -gx ADZERK_ENV prod
-    else
-      set -gx ADZERK_ENV qa
-    end
-  else
-    set -gx ADZERK_ENV qa
-  end
-
-  gpg-source $HOME/.adzerk.fish.asc
-end
-
 function zerkenv
   bass . $CODEDIR/zerkenv/zerkenv.sh $argv
 end
 
+set -gx DOCKER_USER_MODE no
 set -gx ZERKENV_BUCKET zerkenv
+
+set -gx ADZERK_REPO_PATH $CODEDIR/adzerk
+set -gx ADZERK_CLI_TOOLS_REPO_PATH $CODEDIR/cli-tools
+set -gx ADZERK_SCRIPTS_PATH $ADZERK_CLI_TOOLS_REPO_PATH/scripts
+
+balias adzerk $ADZERK_CLI_TOOLS_REPO_PATH/adzerk
+
+function adzerk-ops-mode
+  set -l cli_tools_script_paths alan jarrod jason micha scripts
+
+  for folder in $cli_tools_script_paths
+    set -gx PATH $ADZERK_CLI_TOOLS_REPO_PATH/$folder $PATH
+  end
+end
+
+# Start a new mono-docker container.
+balias monovm $ADZERK_REPO_PATH/docker/run.sh
+
+# Run a command (or start a shell) in a running mono-docker container.
+balias in-monovm $ADZERK_REPO_PATH/docker/attach.sh
 
--- fish/custom/ssh.fish
+++ fish/custom/ssh.fish
@@ -29,6 +29,11 @@ end
 
 # apparently this has to be run once per session now
 ssh-add -A > /dev/null ^&1
+# make sure SSH agent knows about my keys (this should work anyway, but tmux
+# can throw a wrench in things, so this will make sure it always works)
+ssh-add ~/.ssh/*.pem > /dev/null ^&1
+# I can't get this to be non-interactive
+# echo (ssh-add ~/.ssh/id_rsa)
 
 # if gnome-keyring-daemon exists, start it and use the result to set
 # SSH_AUTH_SOCK
