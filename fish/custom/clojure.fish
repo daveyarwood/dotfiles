874d461a839a27338ff54edee4eeabd60b3f47d5
Dave Yarwood <dave.yarwood@gmail.com>
2017-11-27 09:41:10 -0500

    remove UseConcMarkSweepGC from BOOT_JVM_OPTIONS -- deprecated in java 9

--- fish/custom/clojure.fish
+++ fish/custom/clojure.fish
@@ -1,3 +1,3 @@
 set -gx BOOT_HOME $HOME/.config/boot
-set -gx BOOT_JVM_OPTIONS '-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xmx2g -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Xverify:none'
+set -gx BOOT_JVM_OPTIONS '-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xmx2g -XX:+CMSClassUnloadingEnabled -Xverify:none'
 set -gx BOOT_EMIT_TARGET no
