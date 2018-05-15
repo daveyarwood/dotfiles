7952dea8ace09a35ccdbda87dc2a23a3ee23b3cb
Dave Yarwood <dave.yarwood@gmail.com>
2018-05-15 16:27:25 -0400

    Revert "default BOOT_CLOJURE_VERSION to 1.9.0"
    This reverts commit 31b1201d5a6065338ec60c6c17e3cc373745bd5e.

--- fish/custom/clojure.fish
+++ fish/custom/clojure.fish
@@ -1,4 +1,3 @@
 set -gx BOOT_HOME $HOME/.config/boot
 set -gx BOOT_JVM_OPTIONS '-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xmx2g -XX:+CMSClassUnloadingEnabled -Xverify:none'
 set -gx BOOT_EMIT_TARGET no
-set -gx BOOT_CLOJURE_VERSION '1.9.0'
