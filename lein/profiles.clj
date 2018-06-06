2b0330fe54735662e243e8b4e0709d005f4fd9bf
Dave Yarwood <dave.yarwood@gmail.com>
2018-06-06 16:45:47 -0400

    remove lein-ancient dep in profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,6 @@
 {:user {:plugins [[lein-try "0.4.3"]
                   [lein-exec "0.3.4"]
-                  [lein-bin "0.3.4"]
-                  [lein-ancient "0.6.7"]]}
+                  [lein-bin "0.3.4"]]}
  :repl {:plugins []}}
 
 [refactor-nrepl "2.3.0-SNAPSHOT"]
