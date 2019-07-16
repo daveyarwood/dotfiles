1b95fb38ba1035ac45f49e0d69b451bbbc7237c6
Dave Yarwood <dave.yarwood@gmail.com>
2019-07-16 14:27:09 -0400

    use git commit -v to show the diff in vim when i'm editing the commit message

--- fish/custom/git.fish
+++ fish/custom/git.fish
@@ -3,7 +3,7 @@ generate_gitconfig
 balias git hub
 balias g git
 balias gs 'git status'
-balias gc 'git commit'
+balias gc 'git commit -v'
 balias gco 'git checkout'
 function ga; git add $argv; git status; end
 balias gl "git log --graph --pretty=format:\"%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]\" --decorate --all --date=short"
