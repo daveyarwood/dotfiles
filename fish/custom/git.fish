5399b169e57e2df0df3b979425e82dbac9ed6a8d
Dave Yarwood <dave.yarwood@gmail.com>
2019-08-30 11:21:45 -0400

    add gsu alias which does something kind of like git submodule update --remote but only if master branch contains the commit

--- fish/custom/git.fish
+++ fish/custom/git.fish
@@ -36,3 +36,32 @@ function gpl
     git submodule update
   end
 end
+
+# This is kind of like `git submodule update --remote`, except that it tells me
+# what branch(es) contain the commit of each submodule, and updates any
+# submodules whose commits are on the master branch, to point to the latest
+# commit on the master branch.
+function gsu
+  for submodule in (ls submodules)
+    cd submodules/$submodule
+    set commit (git rev-parse HEAD)
+    set branches (git branch -r --contains $commit | sed 's/^[* ]\+//')
+
+    set on_master no
+    for branch in $branches
+      if test $branch = "origin/master"
+	set on_master yes
+      end
+    end
+
+    if test $on_master = yes
+      echo "$submodule is on master branch. Updating..."
+      git checkout master
+      git pull
+      cd -
+    else
+      echo "ERROR: $submodule submodule is on non-master branch: $branches"
+      cd -
+    end
+  end
+end
