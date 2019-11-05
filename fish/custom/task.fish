4f39844fa1243cbd71bec48c17817d56422c1f11
Dave Yarwood <dave.yarwood@gmail.com>
2019-11-05 14:36:57 -0500

    the "clear" in the todo function is sufficient

--- fish/custom/task.fish
+++ fish/custom/task.fish
@@ -6,55 +6,46 @@ end
 balias td todo
 
 function tadd
-  clear
   task add $argv
   todo
 end
 
 function tann
-  clear
   task $argv[1] annotate $argv[2..-1]
   todo
 end
 
 function tdel
-  clear
   task $argv delete
   todo
 end
 
 function tdep
-  clear
   task $argv[1] modify depends:$argv[2]
   todo
 end
 
 function tdone
-  clear
   task $argv done
   todo
 end
 
 function tdue
-  clear
   task $argv[1] modify due:$argv[2]
   todo
 end
 
 function tedit
-  clear
   task $argv edit
   todo
 end
 
 function tmod
-  clear
   task $argv[1] modify $argv[2..-1]
   todo
 end
 
 function tsnooze
-  clear
   task $argv[1] modify wait:$argv[2]
   todo
 end
@@ -62,13 +53,11 @@ end
 balias tsn tsnooze
 
 function tstart
-  clear
   task $argv start
   todo
 end
 
 function tstop
-  clear
   task $argv stop
   todo
 end
