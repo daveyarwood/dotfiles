d5e8cd2a75265b4beffab7002362113d2d3889c2
Dave Yarwood <dave.yarwood@gmail.com>
2020-02-19 14:02:49 -0500

    update rbenv 0.4.0 => 1.1.2-26-gc6324ff

--- fish/custom/rbenv.fish
+++ fish/custom/rbenv.fish
@@ -4,12 +4,6 @@ else
   set -gx RBENV_ROOT $HOME/.rbenv
 end
 
-# In my Ubuntu environments, the script that (rbenv init - | psub) emits is in
-# bash instead of fish for some reason. I don't feel like shaving this yak, so
-# just explicitly use bash on the machines in question.
-if test (hostname) = astrocat -o (hostname) = moondog
-  bash (rbenv init - | psub)
-else
-  source (rbenv init - | psub)
-end
+add-dir-to-path $RBENV_ROOT/bin
 
+source (rbenv init - | psub)
