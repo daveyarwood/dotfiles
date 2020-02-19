fb66429e3eaaa9aa581cfef4fcd39e6231c82aa1
Dave Yarwood <dave.yarwood@gmail.com>
2020-02-19 10:34:09 -0500

    snooze tasks at the start of shell session

--- fish/config.fish
+++ fish/config.fish
@@ -76,7 +76,10 @@ source $fish_runtime/custom/fish_logo.fish
 source $fish_runtime/custom/fish_swimming.fish
 
 function fish_greeting;
-  which task >/dev/null; and task ready 2>/dev/null
+  if which task >/dev/null
+    which snooze-tasks >/dev/null; and snooze-tasks
+    task ready 2>/dev/null
+  end
 end
 
 # erase the default fish_mode_prompt; I have one built into my theme
