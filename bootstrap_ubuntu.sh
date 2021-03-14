91e00e46479aee98694e5949b1fb2a0615a47b9a
Dave Yarwood <dave.yarwood@gmail.com>
2021-03-14 08:45:23 -0400

    Reorganize bootstrap_ubuntu.sh

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -1,115 +1,131 @@
 #!/usr/bin/env bash
 
-update_and_upgrade_apt_packages() {
-  echo Updating/upgrading apt packages...
-  sudo apt update
-  sudo apt upgrade
-}
+################################################################################
+# Install misc. apt packages
+################################################################################
 
-install_git() {
-  sudo apt install git-all
-}
+echo "Updating/upgrading existing apt packages..."
+echo
+sudo apt update
+sudo apt upgrade
 
-install_curl() {
-  sudo apt install curl
-}
+echo
+echo "Installing various apt packages..."
+echo
+sudo apt install \
+  curl \
+  git-all \
+  xsel xclip \
+  rbenv \
+  ack-grep silversearcher-ag \
+  tmux
 
-install_fish_shell() {
-  echo Installing fish shell...
-  sudo apt install fish
+################################################################################
+# Install fish shell
+################################################################################
 
-  echo Making fish the default shell...
-  which fish | sudo tee -a /etc/shells
-  chsh -s "$(which fish)"
-}
+echo
+echo "Installing fish shell..."
+echo
+sudo apt install fish
 
-install_oh_my_fish() {
-  echo Installing oh-my-fish...
-  curl -L http://get.oh-my.fish | fish
-}
+echo
+echo "Making fish the default shell..."
+echo
+which fish | sudo tee -a /etc/shells
+chsh -s "$(which fish)"
 
-install_jenv() {
-  git clone https://github.com/gcuisinier/jenv.git ~/.jenv
-}
+# TODO: Look into what (if anything) I need OMF for and either don't use a fish
+# shell package manager at all, or switch to a newer/maintained one.
+echo
+echo "Installing oh-my-fish..."
+echo
+curl -L http://get.oh-my.fish | fish
 
-install_rbenv() {
-  sudo apt install rbenv
-}
+################################################################################
+# Install jenv
+################################################################################
 
-install_hub() {
-  sudo apt install golang
-  cd /tmp
-  git clone https://github.com/github/hub.git && cd hub
-  script/build -o "$HOME/bin/hub"
-}
+echo
+echo "Installing jenv..."
+echo
+git clone https://github.com/gcuisinier/jenv.git ~/.jenv
 
-install_dotfiles() {
-  # my dotfiles rely on $CODEDIR and $MUSICDIR being set already, which may not
-  # be the case, so we need to set them here if they aren't set
-  if [[ -z $CODEDIR ]]; then
-    export CODEDIR=$HOME/Code
-  fi
+################################################################################
+# Install hub
+################################################################################
 
-  if [[ -z $MUSICDIR ]]; then
-    export MUSICDIR=$HOME/Music
-  fi
+echo
+echo "Installing hub..."
+echo
 
-  git clone https://github.com/daveyarwood/dotfiles.git "$HOME/.dotfiles"
-  cd .dotfiles
-  ./install
-}
+sudo apt install golang
+cd /tmp
+git clone https://github.com/github/hub.git && cd hub
+script/build -o "$HOME/bin/hub"
 
-install_neovim() {
-  sudo apt install software-properties-common
-  sudo add-apt-repository ppa:neovim-ppa/unstable
-  sudo apt update
-  sudo apt install neovim
-  sudo apt install python-dev python-pip python3-dev python3-pip
-  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-  sudo update-alternatives --config vi
-  sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-  sudo update-alternatives --config vim
-  sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-  sudo update-alternatives --config editor
-}
+################################################################################
+# Install Neovim
+################################################################################
 
-install_vim-plug() {
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  vim +PlugInstall +qall
-}
+echo
+echo "Installing Neovim..."
+echo
 
-install_ack() {
-  sudo apt install ack-grep
-}
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim
+sudo apt install python-dev python-pip python3-dev python3-pip
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
 
-install_tmux() {
-  sudo apt install tmux
-}
+################################################################################
+# Install vim-plug
+################################################################################
 
-install_ag() {
-  sudo apt install silversearcher-ag
-}
+echo
+echo "Installing vim-plug..."
+echo
 
-update_and_upgrade_apt_packages
-sudo apt install xsel xclip
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
 
-install_git
-install_curl
-install_ack
-install_ag
-install_tmux
-install_fish_shell
-install_oh_my_fish
-install_jenv
-install_rbenv
-install_hub
-install_dotfiles
-install_neovim
-install_vim-plug
+################################################################################
+# Install dotfiles
+################################################################################
+
+echo
+echo "Installing dotfiles..."
+echo
+
+# my dotfiles rely on $CODEDIR and $MUSICDIR being set already, which may not
+# be the case, so we need to set them here if they aren't set
+if [[ -z $CODEDIR ]]; then
+  export CODEDIR=$HOME/Code
+fi
+
+if [[ -z $MUSICDIR ]]; then
+  export MUSICDIR=$HOME/Music
+fi
+
+git clone https://github.com/daveyarwood/dotfiles.git "$HOME/.dotfiles"
+cd "$HOME/.dotfiles"
+./install
+
+################################################################################
+
+# TODO: Install i3
 
 # TODO: Install a terminal emulator? Unclear which one I really want. Right now
 # I'm using Roxterm, instructions here:
 # https://launchpad.net/~h-realh/+archive/ubuntu/roxterm
+
+################################################################################
 
 echo "Done!"
