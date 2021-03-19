f07dbb02e3d98a5bd7d8c88b6c356e3094af2556
Dave Yarwood <dave.yarwood@gmail.com>
2021-03-18 22:11:01 -0400

    bootstrap_ubuntu.sh: Update Neovim installation steps
    python-pip is no longer available in the apt repos out of the box. Seems in
    general that the community is moving away from supporting Python 2. I'm just
    going to go without the Python 2 Neovim provider and see how far I get.
    
    If it turns out that I find that I really do need the Python 2 Neovim provider,
    then I'll figure out how to add it and update the script accordingly.

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -58,13 +58,15 @@ sudo apt install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
 sudo apt install neovim
-sudo apt install python-dev python-pip python3-dev python3-pip
+sudo apt install python3-dev python3-pip
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
+
+python3 -m pip install --user --upgrade pynvim
 
 ################################################################################
 # Install vim-plug
