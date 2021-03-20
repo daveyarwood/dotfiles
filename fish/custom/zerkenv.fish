a0125a3f90ad0bebdeb2c188bb09d5c8b2e74060
Dave Yarwood <dave.yarwood@gmail.com>
2021-03-20 14:23:32 -0400

    Guard against zerkenv not being installed. I'm going to try something new.

--- fish/custom/zerkenv.fish
+++ fish/custom/zerkenv.fish
@@ -1,7 +1,11 @@
+# TODO: Remove this entirely once I'm done using moondog.
+
 # Setup from README
 set -gx ZERKENV_BUCKET zerkenv
 set -gx ZERKENV_REGION us-east-1
-. (zerkenv -i fish | psub)
+if test (hostname) = moondog
+  . (zerkenv -i fish | psub)
+end
 
 # Convenient short alias
 alias ze zerkenv
