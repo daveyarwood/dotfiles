c1efc9db6364972c376bcba80f33dbbcf6bc306b
Dave Yarwood <dave.yarwood@gmail.com>
2021-03-22 16:08:03 -0400

    Add some instructions to bootstrap_kevel_ubuntu.sh

--- bootstrap_kevel_ubuntu.sh
+++ bootstrap_kevel_ubuntu.sh
@@ -9,3 +9,12 @@ set -eo pipefail
 #   sudo dpkg -i ~/Downloads/slack-desktop-4.14.0-amd64.deb
 echo "*** Install Slack, if you haven't already! ***"
 echo
+
+if ! [[ -f "$HOME/.ssh/kevel.pem" ]]; then
+  echo "*** Obtain ~/.ssh/kevel.pem and ~/.ssh/config ***"
+  echo
+fi
+
+echo "*** Install Datadog agent, using the one-liner here: ***"
+echo "    https://app.datadoghq.com/account/settings#agent/ubuntu"
+echo
