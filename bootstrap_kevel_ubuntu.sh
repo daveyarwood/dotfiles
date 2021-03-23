aa8043c58d7c3e7f5adb674f4a53df8e4bb80f9a
Dave Yarwood <dave.yarwood@gmail.com>
2021-03-23 09:21:22 -0400

    Make Kevel bootstrap script more interactive

--- bootstrap_kevel_ubuntu.sh
+++ bootstrap_kevel_ubuntu.sh
@@ -2,22 +2,28 @@
 
 set -eo pipefail
 
+function press-enter-to-continue() {
+  echo
+  echo "-- Press ENTER to continue --"
+  read
+}
+
 # This part is difficult to automate. It involves clicking a button on the Slack
 # website to download a .deb file (the download URL isn't clear from the
 # markup), then running a command like (actual filename will vary):
 #
 #   sudo dpkg -i ~/Downloads/slack-desktop-4.14.0-amd64.deb
 echo "*** Install Slack, if you haven't already! ***"
-echo
+press-enter-to-continue
 
 if ! [[ -f "$HOME/.ssh/kevel.pem" ]]; then
   echo "*** Obtain ~/.ssh/kevel.pem and ~/.ssh/config ***"
-  echo
+  press-enter-to-continue
 fi
 
 echo "*** Install Datadog agent, using the one-liner here: ***"
 echo "    https://app.datadoghq.com/account/settings#agent/ubuntu"
-echo
+press-enter-to-continue
 
 # ufw (universal firewall) should be enabled by default, which is great, because
 # it's an audit requirement that we all have a firewall enabled.
