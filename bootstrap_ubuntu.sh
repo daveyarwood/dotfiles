b1517446ff3dabac93bb44bfb75c77a0d7b7956f
Dave Yarwood <dave.yarwood@gmail.com>
2021-04-07 15:13:21 -0400

    Install speedtest CLI

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -539,6 +539,22 @@ echo
 pip3 install google_speech
 
 ################################################################################
+# Install Ookla Speedtest CLI
+################################################################################
+
+# Reference: https://www.speedtest.net/apps/cli
+
+sudo apt-get install gnupg1 apt-transport-https dirmngr
+export INSTALL_KEY=379CE192D401AB61
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+echo "deb https://ookla.bintray.com/debian generic main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
+sudo apt-get update
+# Other non-official binaries will conflict with Speedtest CLI
+# Example how to remove using apt-get
+# sudo apt-get remove speedtest-cli
+sudo apt-get install speedtest
+
+################################################################################
 
 echo
 "$(dirname "$0")/bootstrap_kevel_ubuntu.sh"
