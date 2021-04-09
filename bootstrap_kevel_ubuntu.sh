21ced807ce9df696a7ea2c0d3617d3d03fadcc8f
Dave Yarwood <dave.yarwood@gmail.com>
2021-04-09 13:02:47 -0400

    Install nuget

--- bootstrap_kevel_ubuntu.sh
+++ bootstrap_kevel_ubuntu.sh
@@ -168,4 +168,4 @@ sudo apt install -y gnupg ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
-sudo apt install -y mono-devel mono-xsp4
+sudo apt install -y mono-devel mono-xsp4 nuget
