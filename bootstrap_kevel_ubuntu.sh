283bc5fdda292b0128383a60cd9140e839c83456
Dave Yarwood <dave.yarwood@gmail.com>
2021-04-09 12:41:34 -0400

    Install (modern) Mono

--- bootstrap_kevel_ubuntu.sh
+++ bootstrap_kevel_ubuntu.sh
@@ -159,3 +159,13 @@ make javac
 # I don't understand why, but it works for our purposes, so ¯\_(ツ)_/¯
 echo | make srepl
 popd > /dev/null
+echo
+
+# Reference: https://www.mono-project.com/download/stable/
+echo "Installing Mono..."
+echo
+sudo apt install -y gnupg ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt install -y mono-devel
