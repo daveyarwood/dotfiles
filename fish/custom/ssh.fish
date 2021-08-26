905a9c6d31aa0b2e8b35ec6afd40bca9a1ee7f69
Dave Yarwood <dave.yarwood@gmail.com>
2021-08-26 16:31:02 -0400

    Tinkering with SSH setup

--- fish/custom/ssh.fish
+++ fish/custom/ssh.fish
@@ -19,24 +19,28 @@ function __ssh_agent_start -d "start a new ssh agent"
   source $SSH_ENV > /dev/null
 end
 
-if test -z "$SSH_ENV"
-  setenv SSH_ENV $HOME/.ssh/environment
+function ssh-init
+  if test -z "$SSH_ENV"
+    setenv SSH_ENV $HOME/.ssh/environment
+  end
+
+  if not __ssh_agent_is_started
+    __ssh_agent_start
+  end
+
+  # make sure SSH agent knows about my keys (this should work anyway, but tmux
+  # can throw a wrench in things, so this will make sure it always works)
+  #
+  # UPDATE 2021-08-26: I think I don't need this anymore, because
+  # gnome-keyring-daemon is handling it now? Commenting out in order to test my
+  # theory.
+  #
+  # ssh-add ~/.ssh/*.pem >/dev/null 2>&1
+
+  # if gnome-keyring-daemon exists, start it and use the result to set
+  # SSH_AUTH_SOCK
+  which gnome-keyring-daemon >/dev/null
+  if test $status -eq 0
+    eval (gnome-keyring-daemon --start | sed 's/SSH_AUTH_SOCK=/set SSH_AUTH_SOCK /')
+  end
 end
-
-if not __ssh_agent_is_started
-  __ssh_agent_start
-end
-
-# make sure SSH agent knows about my keys (this should work anyway, but tmux
-# can throw a wrench in things, so this will make sure it always works)
-ssh-add ~/.ssh/*.pem >/dev/null 2>&1
-# I can't get this to be non-interactive
-# echo (ssh-add ~/.ssh/id_rsa)
-
-# if gnome-keyring-daemon exists, start it and use the result to set
-# SSH_AUTH_SOCK
-which gnome-keyring-daemon >/dev/null
-if test $status -eq 0
-  eval (gnome-keyring-daemon --start | sed 's/SSH_AUTH_SOCK=/set SSH_AUTH_SOCK /')
-end
-
