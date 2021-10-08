6df0826826b83046484265cf093c93a31f85fc87
Dave Yarwood <dave.yarwood@gmail.com>
2021-10-08 13:03:29 -0400

    Remove verify:none as well, deprecated

--- fish/custom/clojure.fish
+++ fish/custom/clojure.fish
@@ -1,5 +1,5 @@
 set -gx BOOT_HOME $HOME/.config/boot
-set -gx BOOT_JVM_OPTIONS '-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xmx2g -Xverify:none'
+set -gx BOOT_JVM_OPTIONS '-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xmx2g'
 set -gx BOOT_EMIT_TARGET no
 
 add-dir-to-path $HOME/.vim/bundle/vim-iced/bin
