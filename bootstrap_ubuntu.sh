4f2d4e4a45c0d0b20dfb2a181bf03b292fe0970c
Dave Yarwood <dave.yarwood@gmail.com>
2021-12-22 10:54:23 -0500

    Use apt install -y in bootstrap script

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -475,7 +475,7 @@ echo
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
 sudo apt update
-sudo apt install gh
+sudo apt install -y gh
 
 ################################################################################
 # Install delta (fancy git diff viewer)
