e6b50c550b0e049a807007aa421cfc0e4bb39746
Dave Yarwood <dave.yarwood@gmail.com>
2022-08-17 16:29:27 -0400

    More ssh-agent hackery

--- fish/custom/ssh.fish
+++ fish/custom/ssh.fish
@@ -3,14 +3,14 @@
 function __ssh_agent_is_started -d "check if ssh agent is already started"
   if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
     source $SSH_ENV > /dev/null
-      end
+  end
 
-      if test -z "$SSH_AGENT_PID"
-	return 1
-      end
+  if test -z "$SSH_AGENT_PID"
+    return 1
+  end
 
-      ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep -q ssh-agent
-      return $status
+  ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep -q ssh-agent
+  return $status
 end
 
 function __ssh_agent_start -d "start a new ssh agent"
@@ -38,6 +38,18 @@ function ssh-init
   if command -v gnome-keyring-daemon >/dev/null \
     && not ps aux | grep -v grep | grep -q gnome-keyring-daemon
     eval (gnome-keyring-daemon --start | sed 's/SSH_AUTH_SOCK=/set SSH_AUTH_SOCK /')
+  end
+
+  # 2022-08-17: I randomly started having problems with this again today.
+  # SSH_AUTH_SOCK somehow ended up getting set to the wrong value. In general,
+  # I want to trust whatever SSH_AUTH_SOCK gets set to, but I also have to
+  # expect that it will be set to the wrong thing sometimes. So here, I am doing
+  # a simple check to see if it's set to something invalid, and if it is, then
+  # I pick the first file matching the expected pattern and use that instead.
+  if ! ssh-add -l >/dev/null 2>/dev/null
+    set -l dir (find /tmp -type d -iname 'ssh-*' 2>/dev/null | head -n1)
+    set -l file (find $dir -iname 'agent*')
+    set SSH_AUTH_SOCK $file
   end
 
   # 2021-09-09: Stashing some more stuff here that I can try if the above
