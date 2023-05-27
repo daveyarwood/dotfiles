99029bb7d28d77a0513bd9f98aa89b1307278570
Dave Yarwood <dave.yarwood@gmail.com>
2023-05-27 16:55:26 -0400

    `<leader>tf` and `<C-f>` should use `git_files` instead of `find_files`

--- vim/nvim/lua/user/telescope.lua
+++ vim/nvim/lua/user/telescope.lua
@@ -6,6 +6,7 @@ telescope.setup {
     buffers = { theme = "ivy" },
     diagnostics = { theme = "ivy" },
     find_files = { theme = "ivy" },
+    git_files = { theme = "ivy" },
     help_tags = { theme = "ivy" },
     keymaps = { theme = "ivy" },
     live_grep = { theme = "ivy" },
@@ -30,7 +31,18 @@ nmap("<leader>tb", builtin.buffers)
 nmap("<leader>tc", function() builtin.symbols{ sources = {"julia"} } end)
 nmap("<leader>td", builtin.diagnostics)
 nmap("<leader>te", function() builtin.symbols{ sources = {"emoji"} } end)
-nmap("<leader>tf", builtin.find_files)
+-- I had this bound to find_files at first, but that ignores hidden files that
+-- are part of the project, e.g. .node-version, .clj-kondo/. I'm more likely to
+-- want to jump to those files than I am to want to edit files that aren't in
+-- source control.
+--
+-- find_files also includes all of the files in the submodules, which is
+-- occasionally useful, but usually just noisy.
+--
+-- For those times when I do want to edit files not in source control (or files
+-- in submodules), I can either run `:Telescope find_files` or simply navigate
+-- there in my directory browser.
+nmap("<leader>tf", builtin.git_files)
 nmap("<leader>tg", builtin.live_grep)
 nmap("<leader>th", builtin.help_tags)
 nmap("<leader>tm", builtin.keymaps)
@@ -38,4 +50,4 @@ nmap("<leader>tr", builtin.lsp_references)
 nmap("<leader>ts", builtin.lsp_workspace_symbols)
 
 -- Because I jump around so much, it's nice to have a short mapping for this
-nmap("<c-f>", builtin.find_files)
+nmap("<c-f>", builtin.git_files)
