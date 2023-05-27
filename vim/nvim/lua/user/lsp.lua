c10f7bd160800cc855b9d563b7483509bb86f822
Dave Yarwood <dave.yarwood@gmail.com>
2023-05-27 01:14:44 -0400

    Move comment

--- vim/nvim/lua/user/lsp.lua
+++ vim/nvim/lua/user/lsp.lua
@@ -5,8 +5,8 @@
 local lsp_lines = require("lsp_lines")
 lsp_lines.setup()
 
--- Disable virtual_text since it's redundant due to lsp_lines.
 vim.diagnostic.config({
+  -- Disable virtual_text since it's redundant due to lsp_lines.
   virtual_text = false,
 })
 
