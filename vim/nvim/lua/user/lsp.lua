c2b06033c642a28b18f26782adedeeaee9dbbeb9
Dave Yarwood <dave.yarwood@gmail.com>
2023-05-26 22:04:33 -0400

    Trick out my signs column; switch from gitgutter => gitsigns

--- vim/nvim/lua/user/lsp.lua
+++ vim/nvim/lua/user/lsp.lua
@@ -7,6 +7,15 @@ vim.diagnostic.config({
   virtual_text = false,
 })
 
+-- Fancier diagnostic signs in the gutter on the left
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+----- Language servers -----
+
 local lspconfig = require("lspconfig")
 local cmp_lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
