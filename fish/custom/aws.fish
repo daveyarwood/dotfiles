11d8f825cd1418e13cf78551b5e565a5e50e8722
Dave Yarwood <dave.yarwood@gmail.com>
2023-09-18 11:08:38 -0400

    Add workaround for my work setup

--- fish/custom/aws.fish
+++ fish/custom/aws.fish
@@ -1,5 +1,11 @@
 # Ensure that these environment variables aren't still set somehow on shell
 # startup.
-set -e AWS_ACCESS_KEY_ID
-set -e AWS_SECRET_ACCESS_KEY
-set -e AWS_DEFAULT_REGION
+#
+# ...unless AWS_PROFILE is set, in which case I don't want to clear them out.
+# This is to support my work setup where I have a script that I run that sets
+# AWS creds and opens a subshell.
+if test -z "$AWS_PROFILE"
+  set -e AWS_ACCESS_KEY_ID
+  set -e AWS_SECRET_ACCESS_KEY
+  set -e AWS_DEFAULT_REGION
+end
