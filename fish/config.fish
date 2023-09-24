dd2aac044d8e28d0e4bcd17bf54d58e6a803d953
Dave Yarwood <dave.yarwood@gmail.com>
2023-09-24 15:00:47 -0400

    Simplify fish file sourcing setup

--- fish/config.fish
+++ fish/config.fish
@@ -18,34 +18,9 @@ add-dirs-to-path /opt/X11/bin $HOME/.local/bin /snap/bin
 # TODO: come up with some way to have different paths on different hosts
 set -q CODEDIR; or set -gx CODEDIR $HOME/code
 
-# git shortcuts (ga = git add, etc.)
-source $fish_runtime/custom/git.fish
-
-# misc configs (adding dirs to $PATH, etc.)
-source $fish_runtime/custom/aws.fish
-source $fish_runtime/custom/rbenv.fish
-source $fish_runtime/custom/jenv.fish
-source $fish_runtime/custom/clojure.fish
-source $fish_runtime/custom/vim.fish
-source $fish_runtime/custom/go.fish
-source $fish_runtime/custom/rust.fish
-source $fish_runtime/custom/ssh.fish
-source $fish_runtime/custom/kevel.fish
-source $fish_runtime/custom/fzf.fish
-source $fish_runtime/custom/task.fish
-source $fish_runtime/custom/node.fish
-source $fish_runtime/custom/gpg.fish
-source $fish_runtime/custom/i3.fish
-source $fish_runtime/custom/relpipe.fish
-
-# useful shell functions
-source $fish_runtime/custom/functions.fish
-
-# fish ascii art
-source $fish_runtime/custom/fish_logo.fish
-source $fish_runtime/custom/fish_swimming.fish
-
-source $fish_runtime/custom/theme.fish
+ls "$fish_runtime/custom" | while read fish_file
+  source "$fish_runtime/custom/$fish_file"
+end
 
 # vi mode remaps Ctrl-F to go forward a word, and also apparently remaps Ctrl-A
 # and Ctrl-E for some purpose.
