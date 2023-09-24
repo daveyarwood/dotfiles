8f386f9fe6047712e3d2029d0d6196983459f5d5
Dave Yarwood <dave.yarwood@gmail.com>
2023-09-24 16:46:19 -0400

    Add Ruby gem bin dir to PATH
    This seems a little hacky because of the hard-coded version, but it's fine for
    now.

--- fish/custom/ruby.fish
+++ fish/custom/ruby.fish
@@ -4,6 +4,8 @@ else
   set -gx RBENV_ROOT $HOME/.rbenv
 end
 
-add-dirs-to-path $RBENV_ROOT/bin
+add-dirs-to-path \
+  $RBENV_ROOT/bin \
+  $HOME/.local/share/gem/ruby/3.0.0/bin
 
 rbenv init - | source
