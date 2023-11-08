c710ea266566067e884a57bd9a617bf33e58d1be
Dave Yarwood <dyarwood@govspend.com>
2023-11-08 17:01:22 -0500

    Disable rbenv stuff if rbenv not found on PATH

--- fish/custom/ruby.fish
+++ fish/custom/ruby.fish
@@ -1,11 +1,13 @@
-if test (uname) = Darwin
-  set -gx RBENV_ROOT /usr/local/var/rbenv
-else
-  set -gx RBENV_ROOT $HOME/.rbenv
+if command -v rbenv >/dev/null
+  if test (uname) = Darwin
+    set -gx RBENV_ROOT /usr/local/var/rbenv
+  else
+    set -gx RBENV_ROOT $HOME/.rbenv
+  end
+
+  add-dirs-to-path \
+    $RBENV_ROOT/bin \
+    $HOME/.local/share/gem/ruby/3.0.0/bin
+
+  rbenv init - | source
 end
-
-add-dirs-to-path \
-  $RBENV_ROOT/bin \
-  $HOME/.local/share/gem/ruby/3.0.0/bin
-
-rbenv init - | source
