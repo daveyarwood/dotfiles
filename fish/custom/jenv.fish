a41851d5475d0972b7db8bff4e8aebbced2b5a52
Dave Yarwood <dyarwood@govspend.com>
2023-11-08 17:01:03 -0500

    Disable jenv stuff if jenv not found on PATH

--- fish/custom/jenv.fish
+++ fish/custom/jenv.fish
@@ -1,25 +1,27 @@
-# I have no idea what is setting JAVA_HOME (it's nothing in my dotfiles...), but
-# at any rate, `jenv doctor` says that it could interfere with scripts using the
-# Java version set by jenv, if the scripts use JAVA_HOME.
-set -e JAVA_HOME
+if command -v jenv >/dev/null
+  # I have no idea what is setting JAVA_HOME (it's nothing in my dotfiles...),
+  # but at any rate, `jenv doctor` says that it could interfere with scripts
+  # using the Java version set by jenv, if the scripts use JAVA_HOME.
+  set -e JAVA_HOME
 
-set -gx PATH $HOME/.jenv/bin $PATH
-command jenv rehash 2>/dev/null
-set -gx PATH $HOME/.jenv/shims $PATH
+  set -gx PATH $HOME/.jenv/bin $PATH
+  command jenv rehash 2>/dev/null
+  set -gx PATH $HOME/.jenv/shims $PATH
 
-function jenv
-  set cmd $argv[1]
-  set arg ""
-  if test (count $argv) -gt 1
-    set arg $argv[2..-1]
-  end
-
-  switch "$cmd"
-    case enable-plugin rehash shell shell-options
-        set script (jenv "sh-$cmd" "$arg")
-        eval $script
-    case '*'
-        command jenv $cmd $arg
+  function jenv
+    set cmd $argv[1]
+    set arg ""
+    if test (count $argv) -gt 1
+      set arg $argv[2..-1]
     end
+
+    switch "$cmd"
+      case enable-plugin rehash shell shell-options
+          set script (jenv "sh-$cmd" "$arg")
+          eval $script
+      case '*'
+          command jenv $cmd $arg
+      end
+  end
 end
 
