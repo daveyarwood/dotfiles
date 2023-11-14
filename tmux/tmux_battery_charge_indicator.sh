acebff5bc225e08ee40fd5a5892bf16f1ac54c40
Dave Yarwood <dyarwood@govspend.com>
2023-11-14 10:16:46 -0500

    Address shellcheck warnings

--- tmux/tmux_battery_charge_indicator.sh
+++ tmux/tmux_battery_charge_indicator.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 HEART='♥'
 
-if [[ `uname` == 'Linux' ]]; then
-  battery_info=$(upower -i $(upower -e | grep BAT))
-  current_charge=$(echo $battery_info | grep -o 'energy: \+[0-9]\+' | awk '{print $2}')
-  total_charge=$(echo $battery_info | grep -o 'energy-full-design: \+[0-9]\+' | awk '{print $2}')
-  charging=$(echo $battery_info | grep -c 'state: \+charging')
-  fully_charged=$(echo $battery_info | grep -c 'state: \+fully-charged')
+if [[ "$(uname)" == 'Linux' ]]; then
+  battery_info="$(upower -i "$(upower -e | grep BAT)")"
+  current_charge=$(echo "$battery_info" | grep -o 'energy: \+[0-9]\+' | awk '{print $2}')
+  total_charge=$(echo "$battery_info" | grep -o 'energy-full-design: \+[0-9]\+' | awk '{print $2}')
+  charging=$(echo "$battery_info" | grep -c 'state: \+charging')
+  fully_charged=$(echo "$battery_info" | grep -c 'state: \+fully-charged')
 else
-  battery_info=`ioreg -rc AppleSmartBattery`
-  current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
-  charging=$(echo $battery_info | grep -c "\"IsCharging\" = Yes")
-  fully_charged=$(echo $battery_info | grep -c "\"FullyCharged\" = Yes")
-  total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+  battery_info="$(ioreg -rc AppleSmartBattery)"
+  current_charge=$(echo "$battery_info" | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+  charging=$(echo "$battery_info" | grep -c "\"IsCharging\" = Yes")
+  fully_charged=$(echo "$battery_info" | grep -c "\"FullyCharged\" = Yes")
+  total_charge=$(echo "$battery_info" | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
 fi
 
 charged_slots=$(echo "(($current_charge/$total_charge)*10)+1" | bc -l | cut -d '.' -f 1)
@@ -27,9 +27,13 @@ else
   echo -n '#[fg=red]'
 fi
 
-for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
+for _i in $(seq 1 "$charged_slots"); do
+  echo -n "$HEART"
+done
 
-if [[ $charged_slots -lt 10 ]]; then
+if [[ "$charged_slots" -lt 10 ]]; then
   echo -n '#[fg=white]'
-  for i in `seq 1 $(echo "10-$charged_slots" | bc)`; do echo -n "$HEART"; done
+  for _i in $(seq 1 "$(echo "10-$charged_slots" | bc)"); do
+    echo -n "$HEART"
+  done
 fi
