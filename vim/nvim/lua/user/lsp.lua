d9173c3111ff8a110356a9062b3b69ee6e3c05d4
Dave Yarwood <dave.yarwood@gmail.com>
2024-06-08 14:57:36 -0400

    Remove these mappings, which are using deprecated functions, and apparently are also provided by some plugin now?

--- vim/nvim/lua/user/lsp.lua
+++ vim/nvim/lua/user/lsp.lua
@@ -30,8 +30,6 @@ nmap("K", vim.lsp.buf.hover)
 -- not I have a REPL running.
 nmap("gK", vim.lsp.buf.hover)
 nmap("gr", vim.lsp.buf.references)
-nmap("[d", vim.diagnostic.goto_prev)
-nmap("]d", vim.diagnostic.goto_next)
 
 -- lsp_lines can be noisy when there are a lot of findings, so provide an easy
 -- binding to toggle it as needed
