d837e3bd37f452e5147aa0ef76563988bd09ba62
Dave Yarwood <dave.yarwood@gmail.com>
2024-06-10 13:13:26 -0400

    Revise Ruby stuff

--- fish/custom/ruby.fish
+++ fish/custom/ruby.fish
@@ -1,13 +1,10 @@
+set -gx RBENV_ROOT "$HOME/.rbenv"
+
+add-dirs-to-path \
+  "$RBENV_ROOT/bin" \
+  "$RBENV_ROOT/plugins/ruby-build/bin" \
+  "$HOME/.local/share/gem/ruby/3.0.0/bin"
+
 if command -v rbenv >/dev/null
-  if test (uname) = Darwin
-    set -gx RBENV_ROOT /usr/local/var/rbenv
-  else
-    set -gx RBENV_ROOT $HOME/.rbenv
-  end
-
-  add-dirs-to-path \
-    $RBENV_ROOT/bin \
-    $HOME/.local/share/gem/ruby/3.0.0/bin
-
   rbenv init - | source
 end
