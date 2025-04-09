b6229c703d864cef4fa5be6e66bbd58bc46f3496
Dave Yarwood <dave.yarwood@gmail.com>
2025-04-09 00:46:57 -0400

    Update Node from 18 => 22

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -195,10 +195,10 @@ curl -fsSL \
   https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key \
   | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-# Current LTS as of 2023-09-24; update as needed
-node_major=18
+# Current LTS as of 2025-04-09; update as needed
+node_major=22
 
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$node_major.x nodistro main" \
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$node_major.x nodistro main" \
   | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 sudo apt update
