5d7c7a8f3255aac18d897a5c73c750d586861ea1
Dave Yarwood <dave.yarwood@gmail.com>
2025-05-11 19:41:11 -0400

    Disable a couple of problematic CopilotChat mappings

--- vim/nvim/lua/user/copilot.lua
+++ vim/nvim/lua/user/copilot.lua
@@ -19,7 +19,16 @@ require('copilot').setup({
 -- For example config, see:
 -- https://github.com/deathbeam/dotfiles/blob/master/nvim/.config/nvim/lua/config/copilot.lua
 require("CopilotChat").setup {
-  model = 'claude-3.7-sonnet'
+  model = 'claude-3.7-sonnet',
+  mappings = {
+    -- Disable this mapping because it's `<C-l>` by default, and I use that to
+    -- move between Vim windows / tmux panes.
+    reset = { normal = '', insert = '' },
+    -- Disable this mapping because it's `gj` by default, and that conflicts
+    -- with moving the cursor down! I have my `j` mapped to `gj`, so this is a
+    -- problem for me.
+    jump_to_diff = { normal = '' },
+  }
 }
 
 local nmap = require("user.fns").nmap
