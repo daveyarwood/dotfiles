e4d918c4b3a63bca6a2feac675990594e74131df
Dave Yarwood <dave.yarwood@gmail.com>
2026-01-02 09:16:44 -0500

    Use new `winborder` setting to make popup windows look nicer
    This was prompted by nvim-cmp windows suddenly losing their border because of a
    breaking change where they now expect this variable to be set, instead of
    defaulting to rounded:
    
      https://github.com/hrsh7th/nvim-cmp/issues/2210
    
    I found this related discussion on Reddit:
    
      https://www.reddit.com/r/neovim/comments/1jeg8v4/just_merged_an_option_to_control_the_default/
    
    Docs for the new setting:
    
      https://neovim.io/doc/user/options.html#'winborder'

--- vim/nvim/lua/user/config.lua
+++ vim/nvim/lua/user/config.lua
@@ -1,3 +1,10 @@
+-- A newer setting that controls the default border style for floating windows,
+-- such as the ones used by LSP hover and signature help.
+--
+-- See:
+-- https://www.reddit.com/r/neovim/comments/1jeg8v4/just_merged_an_option_to_control_the_default/
+vim.o.winborder = "rounded"
+
 require("user.cmp")
 require("user.copilot")
 require("user.gitsigns")
