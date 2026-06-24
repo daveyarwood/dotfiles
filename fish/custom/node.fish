ce1f45e126f9e4d764ba141769bbcc68de6c129f
Dave Yarwood <dyarwood@govspend.com>
2026-06-24 15:13:53 -0400

    spark is now using Node 24

--- fish/custom/node.fish
+++ fish/custom/node.fish
@@ -1,6 +1,6 @@
 add-dirs-to-path $HOME/npm/bin $HOME/.yarn/bin
 
-# Hacky workaround to automatically switch to Node 22 when entering this
+# Hacky workaround to automatically switch to Node 24 when entering this
 # particular project.
 #
 # This could easily be extended to automatically run `nvm use` when entering any
@@ -15,10 +15,10 @@ function use-the-right-node-version --on-variable PWD
     # Erase any inherited nvm_current_version so that `nvm use` does not
     # short-circuit on the `$ver != $nvm_current_version` check and actually
     # prepends nvm's bin dir to PATH in this shell. Without this, a pane whose
-    # parent shell had already activated Node 22 will see nvm_current_version
+    # parent shell had already activated Node 24 will see nvm_current_version
     # set but no nvm bin dir in PATH, so `node` falls back to the Homebrew one.
     set --erase nvm_current_version
-    nvm use --silent 22.16.0
+    nvm use --silent 24.16.0
   end
 end
 
