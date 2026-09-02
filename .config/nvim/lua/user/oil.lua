da72c1eebd4c9bf04de022e2f102489d4a2aa12d
Dave Yarwood <david.yarwood@fieldguide.io>
2026-09-02 13:47:56 -0400

    Show git-tracked hidden files in oil.nvim

--- .config/nvim/lua/user/oil.lua
+++ .config/nvim/lua/user/oil.lua
@@ -1,6 +1,62 @@
 local nmap = require("user.fns").nmap
 local oil = require("oil")
 
+---- Show git-tracked hidden files ----
+
+-- Reference:
+-- https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#hide-gitignored-files-and-show-git-tracked-hidden-files
+
+-- helper function to parse output
+local function parse_output(proc)
+  local result = proc:wait()
+  local ret = {}
+  if result.code == 0 then
+    for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+      -- Remove trailing slash
+      line = line:gsub("/$", "")
+      ret[line] = true
+    end
+  end
+  return ret
+end
+
+-- build git status cache
+local function new_git_status()
+  return setmetatable({}, {
+    __index = function(self, key)
+      local ignore_proc = vim.system(
+        { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+        {
+          cwd = key,
+          text = true,
+        }
+      )
+      local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+        cwd = key,
+        text = true,
+      })
+      local ret = {
+        ignored = parse_output(ignore_proc),
+        tracked = parse_output(tracked_proc),
+      }
+
+      rawset(self, key, ret)
+      return ret
+    end,
+  })
+end
+local git_status = new_git_status()
+
+-- Clear git status cache on refresh
+local refresh = require("oil.actions").refresh
+local orig_refresh = refresh.callback
+refresh.callback = function(...)
+  git_status = new_git_status()
+  orig_refresh(...)
+end
+
+-------------------------------------
+
 oil.setup {
   -- ID is automatically added at the beginning, and name at the end
   -- See :help oil-columns
@@ -19,7 +75,24 @@ oil.setup {
     -- Disable <C-h> and <C-l> so they don't conflict with vim-tmux-navigator
     ["<C-h>"] = false,
     ["<C-l>"] = false
-  }
+  },
+  view_options = {
+    is_hidden_file = function(name, bufnr)
+      local dir = require("oil").get_current_dir(bufnr)
+      local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+      -- if no local directory (e.g. for ssh connections), just hide dotfiles
+      if not dir then
+        return is_dotfile
+      end
+      -- dotfiles are considered hidden unless tracked
+      if is_dotfile then
+        return not git_status[dir].tracked[name]
+      else
+        -- Check if file is gitignored
+        return git_status[dir].ignored[name]
+      end
+    end,
+  },
 }
 
 nmap("-", oil.open)
