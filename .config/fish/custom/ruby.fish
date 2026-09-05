b656b261c1ae46699cb6899fbf88f0328e9e3912
Dave Yarwood <dave.yarwood@gmail.com>
2026-09-05 19:52:40 -0400

    fish: point gem bin PATH at Ruby 3.3 (system ruby, not 3.0/rbenv)

--- .config/fish/custom/ruby.fish
+++ .config/fish/custom/ruby.fish
@@ -3,7 +3,7 @@ set -gx RBENV_ROOT "$HOME/.rbenv"
 add-dirs-to-path \
   "$RBENV_ROOT/bin" \
   "$RBENV_ROOT/plugins/ruby-build/bin" \
-  "$HOME/.local/share/gem/ruby/3.0.0/bin"
+  "$HOME/.local/share/gem/ruby/3.3.0/bin"
 
 if command -v rbenv >/dev/null
   rbenv init - | source
